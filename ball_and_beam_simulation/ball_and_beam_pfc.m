2ae74b143bf1083b9ff33ab0bc6d535cb4b6a89b
Artur Amaral <37873236+arturamaral2@users.noreply.github.com>
2022-05-29 21:59:05 -0300

    adicionando controladores iniciais

--- ball_and_beam_simulation/ball_and_beam_pfc.m
+++ ball_and_beam_simulation/ball_and_beam_pfc.m
@@ -1,3 +1,8 @@
+clear
+clc
+syms  k1 k2 k0 k3 s;
+
+
 %% Parametros iniciais 
 g = 9.8;
 mb = 0.064;
@@ -8,33 +13,63 @@ d = 0.12;
 delta = 0.2;
 Km = 0.00767;
 Ki = 0.00767;
-Kg = 14 
-Rm = 2.6 
-N_motor = 0.69 
-N_gearbox = 0.85
+Kg = 14 ;
+Rm = 2.6 ;
+N_motor = 0.69 ;
+N_gearbox = 0.85;
 N_total = N_motor + N_gearbox;
 Jv =  (1/2) * mv * L^2;
+
 %%  representação espaço estados 
-a22 = -(  ((Kg^2)*Ki*Km*N_total)/(Rm *(Jv + mb * delta^2 ) ) *(L^2/d^2))
-a23 = - ( ( mb * Jv *g + (mb^2 )*g * delta^2 )  / ((Jv + mb*delta^2)^2))
+
+a22 = -(((Kg^2)*Ki*Km*N_total)/(Rm *(Jv + mb * delta^2 ) ) *(L^2/d^2));
+a23 = - (( mb * Jv *g + (mb^2 )*g * delta^2 )  / ((Jv + mb*delta^2)^2));
 a41 = -5*g/7;
-b21 = ((Kg*Ki*N_total)/(Rm* (Jv + (mb*(delta^2) )))) * (L/d)
+b21 = ((Kg*Ki*N_total)/(Rm* (Jv + (mb*(delta^2) )))) * (L/d);
 
 A = [ 0 1 0  0 ; 
       0  a22  a23 0; 
       0 0 0 1; 
-      a41 0 0 0 ]
+      a41 0 0 0 ];
 
 B = [0 ;
      b21;
      0 ;
-     0]
+     0];
 
-C = [0 0 1 0 ]
+C = [0 0 1 0];
 
-D = [0]
+D = [0];
 
-%% sistema 
+E = eig(A)
+%% sistema em função de transferencia
+
 sys = ss(A,B,C,D)
 [a,b] = ss2tf(A,B,C,D)
-G  = tf(a,b)
+F  = tf(a,b)
+
+%% Projeto de controlador LQR
+
+Q  = eye(4);
+R = 0.7;
+% equação de riccati
+P = icare(A,B,Q,R)
+K = (R^-1)*transpose(B)*P
+K2 = lqr(A,B,Q,R)
+F0 = a(5)/b(5) % funcao de transferencia com S avaliado em zero
+M = (F0^-1)*transpose(K)*(K*transpose(K))^-1
+
+
+%% Projeto controlador alocação de polos
+
+p= [-0.5 -1.5 -3 -5]
+K_alocacao = place(A,B,p)
+l = place(A',C',p)
+M_alocacao = (F0^-1)*transpose(K_alocacao)*(K_alocacao*transpose(K_alocacao))^-1
+
+%K_alocacao = [k0 k1 k2 k3]
+%I = eye(4)
+%matriz_alocacao = (s*I - (A - B*K_alocacao))
+%polinomio_alocacao = det(matriz_alocacao)
+%eqn = polinomio_alocacao == 0
+%polos_alocao = solve(eqn,s)
